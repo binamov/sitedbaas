f2a85bf736586b0af3be07b3c80f81377f00c098
Nick Rycar <rycar@chef.io>
2015-05-29 13:13:32 -0700

    Recipe updated and delivery config added.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'b@chef.io'
 license 'all_rights'
 description 'Installs/Configures sitedbaas'
 long_description 'Installs/Configures sitedbaas'
-version '0.1.0'
+version '0.1.1'
 
 depends 'httpdbaas'
